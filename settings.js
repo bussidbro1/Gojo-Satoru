99a9ca69a96b9479f81eb51ee3c2818cc210e4e8
bussidbro1 <114237994+bussidbro1@users.noreply.github.com>
2022-09-23 19:23:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94756728780']
+global.premium = ['94756728780']
+global.ownernomer = '94756728780'
+global.ownername = '🐦dulanjana'
+global.botname = 'help full bot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
